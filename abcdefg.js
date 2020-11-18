19161cbabaa3104ada51c27a708b3677ca64fe1b
sixteendigitname <72978538+sixteendigitname@users.noreply.github.com>
2020-11-18 13:56:24 +0530

    eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
    fffffffff
    ff
    ffffff
    ff
    ff

---
+++ abcdefg.js
@@ -0,0 +1,17 @@
+class hello{
+
+constructor(ay, bee){
+
+var option = {
+
+bodyA : ay,
+bodyB : bee,
+stiffness : 1,
+length : 10
+}
+
+this.aa = constraint.create(option)
+World.add(world ,this.aa)
+
+}
+}
